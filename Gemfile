25cff2db345d1a048e3650dbc0dad8b6aa0beb79
Daniel Rubio <drubio1989@gmail.com>
2025-04-03 12:43:13 -0500

    Complete transfer of data fetching

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ end
 
 gem "tailwindcss-rails", "~> 4.2"
 gem 'htmlbeautifier'
+gem 'httparty'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    response = HTTParty.get(Rails.application.credentials.aws.lambda.ipo_calendar)
+    @ipo_calendar_data = JSON.parse(response["body"])
   end
 end
