bbee68f8f692136df88046bd9818498a6af248e2
Daniel Rubio <drubio1989@gmail.com>
2025-04-07 15:43:56 -0500

    refactor form submission with turbo frame

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,26 @@ class HomeController < ApplicationController
   def index
     @ipo_calendar_data = HTTParty.get(ENV['IPO_CALENDAR_LAMBDA_URL'])
   end
+
+  def subscribe
+    email = params[:email]
+
+    if email.blank?
+      @errors = { email: ["can't be blank"] }
+      render partial: "subscribe_form", status: :unprocessable_entity
+      return
+    end
+    
+    response = HTTParty.post(
+      ENV['SUBSCRIBE_EMAIL_LAMBDA_URL'], 
+      body: JSON.generate({ email: params[:email] }), 
+      headers: { 'Content-Type' => 'application/json' }
+    )
+
+    @message = "Thank you for subscribing!"
+
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: "home#index"
+
+  post "subscribe", to: "home#subscribe"
 end
