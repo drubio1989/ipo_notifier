3695b1332e1b39bdc610733ba27032211c8ad530
Daniel Rubio <drubio1989@gmail.com>
2025-05-22 18:00:51 -0500

    Add confirmation message

--- app/controllers/ses_notifications_controller.rb
+++ app/controllers/ses_notifications_controller.rb
@@ -1,41 +1,59 @@
-# app/controllers/ses_notifications_controller.rb
 class SesNotificationsController < ApplicationController
-  # Skip CSRF check since SNS won't send your Rails CSRF token
   skip_before_action :verify_authenticity_token
-
   require 'net/http'
   require 'uri'
 
   def receive
     sns_message = JSON.parse(request.body.read)
 
-    case sns_message['notificationType']
-    when 'Bounce'
-      handle_bounce(sns_message['notificationType']['bounce'])
-    when 'Complaint'
-      handle_complaint(sns_message['notificationType']['complaint'])
+    case sns_message['Type']
+    when 'SubscriptionConfirmation'
+      confirm_subscription(sns_message)
+    when 'Notification'
+      notification = JSON.parse(sns_message['Message'])
+      case notification['notificationType']
+      when 'Bounce'
+        handle_bounce(notification['bounce'])
+      when 'Complaint'
+        handle_complaint(notification['complaint'])
+      else
+        Rails.logger.warn "Unhandled notificationType: #{notification['notificationType']}"
+      end
+      head :ok
     else
+      Rails.logger.warn "Unhandled SNS message type: #{sns_message['Type']}"
       head :bad_request
     end
-  rescue JSON::ParserError
+  rescue JSON::ParserError => e
+    Rails.logger.error "JSON parse error: #{e.message}"
     head :bad_request
+  rescue => e
+    Rails.logger.error "Unexpected error: #{e.message}"
+    head :internal_server_error
   end
 
   private
 
+  def confirm_subscription(message)
+    subscribe_url = message['SubscribeURL']
+    uri = URI.parse(subscribe_url)
+    Net::HTTP.get(uri)
+    Rails.logger.info "Confirmed SNS subscription"
+    head :ok
+  rescue => e
+    Rails.logger.error "Failed to confirm subscription: #{e.message}"
+    head :internal_server_error
+  end
+
   def handle_bounce(bounce)
     bounced_emails = bounce['bouncedRecipients'].map { |r| r['emailAddress'] }
-    Rails.logger.info("Bounced emails: #{bounced_emails.inspect}")
-
-    # TODO: Your logic here: e.g., mark these emails as bounced in your DB
-    # Example:
-    # User.where(email: bounced_emails).update_all(bounced: true)
+    Rails.logger.info "Bounced emails: #{bounced_emails.inspect}"
+    # TODO: Add your bounce handling logic here (e.g., mark emails in DB)
   end
 
   def handle_complaint(complaint)
     complained_emails = complaint['complainedRecipients'].map { |r| r['emailAddress'] }
-    Rails.logger.info("Complaint emails: #{complained_emails.inspect}")
-
-    # TODO: Your logic here: e.g., mark these emails as complained in your DB
+    Rails.logger.info "Complaint emails: #{complained_emails.inspect}"
+    # TODO: Add your complaint handling logic here (e.g., mark emails in DB)
   end
 end
