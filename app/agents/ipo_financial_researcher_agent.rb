a9d5089a192d02fb5629d14fb89fb9a58c307cf6
Daniel Rubio <drubio1989@gmail.com>
2025-10-10 15:38:27 -0500

    remove research agent

--- app/agents/ipo_financial_researcher_agent.rb
+++
@@ -1,9 +0,0 @@
-class IpoFinancialResearcherAgent < ApplicationAgent
-  generate_with :openai, model: "gpt-4o-mini", instructions: "You are a financial reseacher."
-  
-  def research
-    @message = "Cats go.."
-
-    prompt message: @message
-  end
-end
